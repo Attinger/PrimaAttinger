9326de2ea4e50c9fd5abcf16122af83f246905c6
Kevin Attinger <kevin.attinger@hs-furtwangen.de>
2021-10-19 14:10:05 +0200

    implement movement

--- Laserleague/Script/Build/Script.js
+++ Laserleague/Script/Build/Script.js
@@ -1,1 +1,72 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    let transform;
+    let agentMove;
+    function start(_event) {
+        alert('Move around with your arrow keys on your keyboard');
+        viewport = _event.detail;
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        let graph = viewport.getBranch();
+        let laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser_one")[0];
+        let agent = graph.getChildrenByName("Agent")[0].getChildrenByName("Agent_one")[0];
+        transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+        agentMove = agent.getComponent(ƒ.ComponentTransform).mtxLocal;
+        ƒ.Loop.start();
+    }
+    function update(_event) {
+        transform.rotateZ(5);
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP])) {
+            agentMove.translateY(0.1);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+            agentMove.rotateZ(-5);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+            agentMove.rotateZ(5);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN])) {
+            agentMove.translateY(-0.1);
+        }
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Laserleague/Script/Source/Main.ts
+++ Laserleague/Script/Source/Main.ts
@@ -5,16 +5,44 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+  let transform: ƒ.Matrix4x4;
+  let agentMove: ƒ.Matrix4x4
+
   function start(_event: CustomEvent): void {
+    alert('Move around with your arrow keys on your keyboard');
     viewport = _event.detail;
 
+    
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+
+   
+    let graph: ƒ.Node = viewport.getBranch();
+    let laser: ƒ.Node = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser_one")[0];
+    let agent: ƒ.Node = graph.getChildrenByName("Agent")[0].getChildrenByName("Agent_one")[0];
+    transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+    agentMove = agent.getComponent(ƒ.ComponentTransform).mtxLocal;
+
+    ƒ.Loop.start();
+
   }
 
   function update(_event: Event): void {
-    // ƒ.Physics.world.simulate();  // if physics is included and used
+    transform.rotateZ(5);
     viewport.draw();
     ƒ.AudioManager.default.update();
+
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP])){
+      agentMove.translateY(0.1);
+    }
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])){
+      agentMove.rotateZ(-5);
+    }
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])){
+      agentMove.rotateZ(5);
+    }
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN])){
+      agentMove.translateY(-0.1);
+    }
   }
+
 }
